b8e64b4c108ca1c5d5bf7e03ccd16e36945724ec
Sharnika <sharnikrishnan06@gmail.com>
2026-06-18 10:45:56 +0530

    Added test cases

---
+++ test.py
@@ -0,0 +1,8 @@
+def test_citizen_service():
+    assert "Citizen Service Request Submitted" == "Citizen Service Request Submitted"
+
+def test_authentication():
+    assert "Login Successful" == "Login Successful"
+
+def test_data_handling():
+    assert "Citizen Data Stored Successfully" == "Citizen Data Stored Successfully"
